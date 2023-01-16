4bb139cf0eea12138bac667b1c996494c59923f6
t6nesu00 <hlw.zigri@gamil.com>
2023-01-16 19:18:17 +0200

    weather result with city name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 
-import Input from "./components/InputField.js";
 import WeatherResult from "./components/WeatherResult.js";
 
 function App() {
   return (
     <div className="App">
-      <Input />
       <WeatherResult />
     </div>
   );
--- src/components/WeatherResult.js
+++ src/components/WeatherResult.js
@@ -1,15 +1,32 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./weatherResult.css";
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { SearchIcon } from "@chakra-ui/icons";
+import { 
+    IconButton, 
+    Input,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText, 
+} from '@chakra-ui/react';
+import "./input.css";
 
 const WeatherResult = () => {
 
-    const [data, setData] = useState([]);
-    const cityName = "London";
+
+    const [cityName, setCityName] = useState("");
+    const [data, setData] =useState();
+  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getWeatherResult(cityName);
+    }
+
     const apiKey = process.env.REACT_APP_API_KEY;
-    const getWeatherResult = (cityName) => {
-        const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=" + apiKey;
+    const getWeatherResult = (name) => {
+        const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
         axios.get(baseURL)
             .then((res) => {
                 setData(res.data);
@@ -18,25 +35,31 @@ const WeatherResult = () => {
                 console.log(`Error: ${err}`)
             })
     }
-
-    useEffect(() => {
-        getWeatherResult("London");
-    }, [])
-    
     return(
         <div className="main">
+
+        <div className="searchField">
+            <FormControl className='inputForm'>
+                <Input placeholder="search by city" value={cityName} onChange={(e) => setCityName(e.target.value)} size="md" width="auto" style={{ marginRight : 10 }} />
+                <IconButton 
+                    aria-label='Search database'
+                    type='submit' 
+                    icon={<SearchIcon />} 
+                    onClick = {handleSubmit}
+                    />
+            </FormControl>
+        </div>
             <div className="main-data">
-                <h1>{data.name}</h1>
+                <h1>{data?.name}</h1>
                 <div className="reading">
-                    <h3>{data.main.temp} °F</h3>
-                    <h4>Feels like {data.main.feels_like} °F</h4>
+                    <h3>{((data?.main?.temp) - 273.15).toFixed(2)} °C</h3>
+                    <h4>Feels like {((data?.main?.feels_like) -273.15).toFixed(2)} °C</h4>
             </div>
                 <div className="desccription">
-                    <h3>{data.weather[0].main}</h3>
+                    <h3>{data?.weather[0].main}</h3>
                 </div>
             </div>
         </div>
     );
 }
-
 export default WeatherResult;
